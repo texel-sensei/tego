d14c8f65c4f2adf29ad99e335ce0aa082860e951
texel <texelsensei@posteo.net>
2021-10-28 21:56:30 +0200

    refactor: update rasterizer example to use new layer iterator
    The rasterizer now uses the layer_iter() function of the Map instead of
    a recursive function.

--- examples/rasterizer.rs
+++ examples/rasterizer.rs
@@ -19,33 +19,26 @@ impl tego::ImageLoader for ImageLoader {
 }
 
 fn render_layer(
-    map: &tego::Map, layer: &tego::Layer, buffer: &mut RgbaImage
+    map: &tego::Map, layer: &tego::TileLayer, buffer: &mut RgbaImage
 ) -> Result<(), Box<dyn Error>> {
-    use tego::Layer::*;
-    match layer {
-        Group(group) => for l in &group.content { render_layer(map, &l, buffer)?;},
-        Tile(tiles) => {
-            for (pos, gid) in tiles.tiles_in_renderorder(map) {
-                if gid.is_none() { continue; }
-                let (img_path, src_rect) = map.tile_image(gid.unwrap()).unwrap();
+    for (pos, gid) in layer.tiles_in_renderorder(map).filter(|t| t.1.is_some()) {
+        let (img_path, src_rect) = map.tile_image(gid.unwrap()).unwrap();
 
-                // TODO(texel, 2021-10-23): this opens the spritesheet for every single tile,
-                // implement some caching (ideally inside of the map loader)
-                let tile_image = img_path.downcast_ref::<RgbaImage>().unwrap();
+        // TODO(texel, 2021-10-23): this opens the spritesheet for every single tile,
+        // implement some caching (ideally inside of the map loader)
+        let tile_image = img_path.downcast_ref::<RgbaImage>().unwrap();
 
-                let tile_sprite = tile_image.view(
-                    src_rect.upper_left.x as u32, src_rect.upper_left.y as u32,
-                    src_rect.size.x as u32, src_rect.size.y as u32
-                );
+        let tile_sprite = tile_image.view(
+            src_rect.upper_left.x as u32, src_rect.upper_left.y as u32,
+            src_rect.size.x as u32, src_rect.size.y as u32
+        );
 
-                let origin = pos *  map.tile_size;
+        let origin = pos *  map.tile_size;
 
-                buffer.copy_from(
-                    &tile_sprite,
-                    origin.x as u32, origin.y as u32
-                )?;
-            }
-        }
+        buffer.copy_from(
+            &tile_sprite,
+            origin.x as u32, origin.y as u32
+        )?;
     }
     Ok(())
 }
@@ -65,8 +58,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let resolution = map.size * map.tile_size;
     let mut buffer = RgbaImage::new(resolution.x as u32, resolution.y as u32);
 
-    for layer in &map.layers {
-        render_layer(&map, &layer, &mut buffer)?;
+    for layer in map.iter_layers() {
+        use tego::Layer::*;
+        if let Tile(tile_layer) = layer {
+            render_layer(&map, &tile_layer, &mut buffer)?;
+        }
     }
 
     buffer.save(output)?;

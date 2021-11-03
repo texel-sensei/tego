9baf69a4063e2ec51487b400aa22557d6a850c4e
texel <texelsensei@posteo.net>
2021-11-03 19:48:55 +0100

    refactor: change ivec2 definition to use a macro and add fvec2
    The type ivec2 is now defined via a macro. This allows us to easily add
    a fvec2 type that behaves similarly. This type is required for objects,
    as their positions and sizes are floating point numbers.

--- src/math.rs
+++ src/math.rs
@@ -2,22 +2,35 @@
 
 use std::ops;
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Default)]
-#[allow(non_camel_case_types)]
-pub struct ivec2 {
-    pub x: i32,
-    pub y: i32,
+macro_rules! define_vector {
+    ($name:ident $T:ty) => {
+        #[derive(Debug, PartialEq, Copy, Clone, Default)]
+        #[allow(non_camel_case_types)]
+        pub struct $name {
+            pub x: $T,
+            pub y: $T,
+        }
+
+        impl $name {
+            pub fn new(x: $T, y: $T) -> Self { Self { x, y } }
+        }
+
+        impl_op_ex!{+ |a: &$name, b: &$name| -> $name { $name::new(a.x + b.x, a.y + b.y )}}
+        impl_op_ex!{- |a: &$name, b: &$name| -> $name { $name::new(a.x - b.x, a.y - b.y )}}
+        impl_op_ex!{* |a: &$name, b: &$name| -> $name { $name::new(a.x * b.x, a.y * b.y )}}
+        impl_op_ex!{/ |a: &$name, b: &$name| -> $name { $name::new(a.x / b.x, a.y / b.y )}}
+    };
 }
 
-impl ivec2 {
-    pub fn new(x: i32, y: i32) -> Self { Self { x, y } }
-}
-
-impl_op_ex!{+ |a: &ivec2, b: &ivec2| -> ivec2 { ivec2::new(a.x + b.x, a.y + b.y )}}
-impl_op_ex!{- |a: &ivec2, b: &ivec2| -> ivec2 { ivec2::new(a.x - b.x, a.y - b.y )}}
-impl_op_ex!{* |a: &ivec2, b: &ivec2| -> ivec2 { ivec2::new(a.x * b.x, a.y * b.y )}}
-impl_op_ex!{/ |a: &ivec2, b: &ivec2| -> ivec2 { ivec2::new(a.x / b.x, a.y / b.y )}}
+// Define vector of integers
+define_vector!{ivec2 i32}
 impl_op_ex_commutative!{* |a: &ivec2, f: &i32| -> ivec2 { ivec2::new(a.x * f, a.y * f )}}
+impl Eq for ivec2 {}
+
+// Define vector of floats
+define_vector!{fvec2 f32}
+impl_op_ex_commutative!{* |a: &fvec2, f: &f32| -> fvec2 { fvec2::new(a.x * f, a.y * f )}}
+
 
 /// Struct that defines a rectangle given by its upper left corner and extends.
 #[derive(Debug, PartialEq, Eq)]

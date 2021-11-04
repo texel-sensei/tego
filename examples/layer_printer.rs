9bf4b56bd6c95e9ab34f54e7f56395fced35ca0a
texel <texelsensei@posteo.net>
2021-11-04 18:07:36 +0100

    docs: add simple example to print the layers in a map
    Add a new basic example that loads a map and prints all layers included
    in it. This example is also intended to be used in the readme.

---
+++ examples/layer_printer.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+extern crate tego;
+
+fn main() -> tego::Result<()> {
+    // Load a tmx file.
+    // Map::from_file() is the easiest, but least flexible method for loading a map.
+    // Images referenced by the map are not loaded, instead only the path is returned as string.
+	let map = tego::Map::from_file(Path::new("example-maps/default/groups.tmx"))?;
+
+    // Keep track how much we need to indent for some nice pretty printing
+	let mut indent = 0;
+
+	for (layer, groups_left) in map.iter_layers() {
+        // Reduce indentation by the amount of groups left
+        indent -= groups_left;
+
+        // print indentation to highlight hierarchy
+        print!("{}", "  ".repeat(indent));
+
+        use tego::Layer::*;
+        match layer {
+            Tile(layer) => {
+                println!("Layer '{}' with {}x{} tiles", layer.name, layer.size.x, layer.size.y);
+            },
+            Group(layer) => {
+                println!("Group layer '{}' with {} sub-layers", layer.name, layer.content.len());
+
+                // increase indentation for all layers part of this group
+                indent += 1;
+            },
+            Object(layer) => {
+                println!("Layer '{}' containing {} objects", layer.name, layer.content.len());
+            },
+        }
+	}
+	Ok(())
+}
